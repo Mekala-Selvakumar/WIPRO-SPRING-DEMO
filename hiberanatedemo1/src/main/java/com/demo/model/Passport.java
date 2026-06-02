4a946d6f4a52fad2fc0312803a850af4b8749927
Admin <mekala.selvakumar.2021@gmail.com>
2026-06-02 10:46:16 +0530

    project added

--- hiberanatedemo1/src/main/java/com/demo/model/Passport.java
+++ hiberanatedemo1/src/main/java/com/demo/model/Passport.java
@@ -11,38 +11,46 @@ public class Passport {
 	private int passportId;
 	private String passportNumber;
 	@OneToOne
-	@JoinColumn(name="person_id")
- private Person person;
+	@JoinColumn(name = "person_id")
+	private Person person;
+
 	public Passport() {
 		// TODO Auto-generated constructor stub
 	}
+
 	public Passport(int passportId, String passportNumber) {
 		super();
 		this.passportId = passportId;
 		this.passportNumber = passportNumber;
 	}
+
 	public int getPassportId() {
 		return passportId;
 	}
+
 	public void setPassportId(int passportId) {
 		this.passportId = passportId;
 	}
+
 	public String getPassportNumber() {
 		return passportNumber;
 	}
+
 	public void setPassportNumber(String passportNumber) {
 		this.passportNumber = passportNumber;
 	}
+
 	public Person getPerson() {
 		return person;
 	}
+
 	public void setPerson(Person person) {
 		this.person = person;
 	}
+
 	@Override
 	public String toString() {
 		return "Passport [passportId=" + passportId + ", passportNumber=" + passportNumber + ", person=" + person + "]";
 	}
-	
 
 }
